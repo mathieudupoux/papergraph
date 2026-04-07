3e59860234d52d8bcd7950e613c7ed04b5151f85
Remy Vallot <remy.vallot02@gmail.com>
2026-04-07 14:47:03 +0200

    vite error import module

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,19 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/', 
+  base: '/', // On reste sur la racine pour papergraph.net
   root: '.',
-  publicDir: 'public',
   build: {
     outDir: 'dist',
-    // --- ON CHANGE LE NOM DU DOSSIER GÉNÉRÉ ICI ---
-    assetsDir: 'build-assets', 
+    assetsDir: 'build-assets', // Évite le conflit avec ton dossier 'assets' de logos
     rollupOptions: {
       input: {
-        editor: resolve(__dirname, 'editor.html'),
         index: resolve(__dirname, 'index.html'),
+        editor: resolve(__dirname, 'editor.html'),
         gallery: resolve(__dirname, 'gallery.html'),
         projects: resolve(__dirname, 'projects.html'),
         privacy: resolve(__dirname, 'privacy.html'),
       },
     },
-  },
-  server: {
-    port: 8000,
   },
 });
